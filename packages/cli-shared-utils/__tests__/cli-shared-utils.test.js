df3fb20e1614b939a66712f2ea8235af717a751f
林宇森 <linyusen@dslyy.com>
2023-02-21 17:22:21 +0800

    fix: del

--- packages/cli-shared-utils/__tests__/cli-shared-utils.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const cliSharedUtils = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(cliSharedUtils(), 'Hello from cliSharedUtils');
-console.info('cliSharedUtils tests passed');
